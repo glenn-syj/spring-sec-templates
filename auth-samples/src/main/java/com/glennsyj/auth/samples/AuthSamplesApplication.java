46c6976a36d6b8102c6c5d6fa6fadd89f3c386c3
glenn-syj <glenn.syj@gmail.com>
2024-09-07 22:59:48 +0900

    init: initialize a spring boot project
    add dependencies for authentication and authorization

---
+++ auth-samples/src/main/java/com/glennsyj/auth/samples/AuthSamplesApplication.java
@@ -0,0 +1,13 @@
+package com.glennsyj.auth.samples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthSamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthSamplesApplication.class, args);
+	}
+
+}
---
+++ auth-samples/src/test/java/com/glennsyj/auth/samples/AuthSamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glennsyj.auth.samples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthSamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
