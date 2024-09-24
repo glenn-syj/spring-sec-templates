b35d6557e616d2cf6bde8747fd24ad4b25a5066a
glenn.syj <glenn.syj@gmail.com>
2024-09-24 17:40:42 +0900

    feat: add a ClientProperties to utilize the configuration variables with OAuth

--- auth-samples/src/main/java/com/glennsyj/auth/samples/config/ClientProperties.java
+++ auth-samples/src/main/java/com/glennsyj/auth/samples/config/ClientProperties.java
@@ -4,30 +4,44 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 import lombok.Getter;
+import lombok.Setter;
 
-@Configuration
-@ConfigurationProperties(prefix = "oauth2.clients")
-
+@Component
+@ConfigurationProperties(prefix = "spring.security.oauth2.client")
 public class ClientProperties {
 
-	private Map<String, ClientRegistrationProperties> clients = new HashMap<>();
+	private Map<String, ClientRegistrationProperties> registration = new HashMap<>();
+	private Map<String, ProviderProperties> provider = new HashMap<>();
 
-	public Map<String, ClientRegistrationProperties> getClients() {
-		return clients;
+	public Map<String, ClientRegistrationProperties> getRegistration() {
+		return registration;
+	}
+
+	public Map<String, ProviderProperties> getProvider() {
+		return provider;
 	}
 
 	@Getter
+	@Setter
 	public static class ClientRegistrationProperties {
 		private String clientId;
 		private String clientSecret;
+		private String scope;
+		private String authorizationGrantType;
+		private String redirectUri;
+		private String clientName;
+		private String clientAuthenticationMethod;
+	}
+
+	@Getter
+	@Setter
+	public static class ProviderProperties {
 		private String authorizationUri;
 		private String tokenUri;
 		private String userInfoUri;
-		private String redirectUri;
-		private String scope;
+		private String userNameAttribute;
 	}
 }
-
